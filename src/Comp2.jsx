3dd5dc8f7c07826d03cd9587b74d9a3aff9bfd97
Dipan Sarkar <alexmithun1005@gmail.com>
2024-07-15 10:52:46 +0530

    layout fixes

--- src/Comp2.jsx
+++ src/Comp2.jsx
@@ -1,29 +1,30 @@
-import React, { useRef } from 'react'
-import Navbar from './Components/Navbar'
-import HeroPart from './Components/HeroPart'
-
-
+import  {  useRef  } from "react";
+import Navbar from "./Components/Navbar";
+import HeroPart from "./Components/HeroPart";
+import { motion } from "framer-motion";
+import SocialMediaBox from "./Components/SocialMediaBox";
 
 function Comp2() {
-  const clientX=  useRef(0)
-  const clientY=  useRef(0)
+  const pointerPosition = useRef({ x: 0, y: 0 });
 
-  const pointerMove = (e)=>{
-    console.log((e.clientX , e.clientY));
-    clientX.current = e.clientX
-    clientY.current = e.clientY
-    console.log(clientX , clientY)
-    }
-  return (
-   
-    <div className="comp" id="comp2" onMouseMove={(e)=>pointerMove(e)}>
-    <Navbar/>
-<HeroPart/>
-<div className="pointer" style={{top:clientY , left:clientX} }></div>
+  function pointerMove(e) {
+    console.log(e.clientX, e.clientY);
+    pointerPosition.current = { x: e.clientX, y: e.clientY };
+    console.log(pointerPosition.current.x);
+  }
 
-
-  </div>
-  )
+  return (<>
+    <div className="comp" id="comp2" onMouseMove={(e) => pointerMove(e)}>
+      <Navbar />
+      <HeroPart />
+      <motion.div
+        animate={{ x: pointerPosition.current.x, y: pointerPosition.current.y , transition:{type:'spring' , mass:3} }}
+        className="pointer"
+        ></motion.div>
+<SocialMediaBox/>
+    </div>
+        </>
+  );
 }
 
-export default Comp2
+export default Comp2;
--- src/Components/HeroPart.jsx
+++ src/Components/HeroPart.jsx
@@ -1,49 +1,53 @@
-import React from 'react'
-import HeroPhoto from './HeroPhoto'
-import { Typewriter } from 'react-simple-typewriter'
-
+import React from "react";
+import HeroPhoto from "./HeroPhoto";
+import { Typewriter } from "react-simple-typewriter";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SocialMediaBox from "./SocialMediaBox";
 
 function HeroPart() {
   return (
+    <>
     <div className="heroBanner">
-    <div className="HeroPart">
-        <HeroPhoto/>
+      <div className="HeroPart">
+        <HeroPhoto />
         <p className="description">
-Hello, I’m <span>Dipan Sarkar</span> UX/UI Designer and  <br />
-Front-end Developer Based in India❤️.
-</p>
+          Hello, I’m <span>Dipan Sarkar</span> UX/UI Designer and <br />
+          Front-end Developer Based in India❤️.
+        </p>
+        <SocialMediaBox />
+      </div>
 
-    </div>
-
-    <div className="heroFooter">
-<div className="contactInfo">
-    <p>
-        Email : dipansarkar1005@gmail.com
-    </p>
-    <a href='tel'>
-        call : +91 7003224488
-    </a>
-
-</div>
-<h1 className='Typewriter'>I  {" "}
-    <span>
-
-<Typewriter
-            words={['Am A Developer', 'Code Your Thoughts', 'Build Web Apps' , "Code Cool Websites"]}
+      <div className="heroFooter">
+        <div className="contactInfo">
+          <p>Email : dipansarkar1005@gmail.com</p>
+          <a href="tel">call : +91 7003224488</a>
+        </div>
+        <h1 className="Typewriter">
+          I{" "}
+          <span>
+            <Typewriter
+              words={[
+                "Am A Developer",
+                "Code Your Thoughts",
+                "Build Web Apps",
+                "Code Cool Websites",
+            ]}
             loop={10000}
             cursor
-            cursorStyle='|'
+            cursorStyle="|"
             typeSpeed={70}
             deleteSpeed={50}
             delaySpeed={1000}
             onLoopDone={console.log("done")}
             onType={console.log("type")}
             />
-            </span>
-</h1>
+          </span>
+        </h1>
+      </div>
     </div>
-    </div>
-  )
+    
+            </>
+  );
 }
 
-export default HeroPart
+export default HeroPart;
