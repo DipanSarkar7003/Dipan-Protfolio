1f1a294a3f174ac03c1c4cc49c1be874da925545
Dipan Sarkar <alexmithun1005@gmial.com>
2024-10-25 11:48:56 +0530

    work section complete

--- src/Components/Comp3.jsx
+++ src/Components/Comp3.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Comp3() {
   return (
-    <div id='comp3' className='comp' style={{color:"white"}}>Comp3</div>
+    <div id='comp3' className='comp' style={{color:"white"}}></div>
   )
 }
 
